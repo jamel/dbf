4c9dffa98415045abcbb448dfc6438164e15bc67
AlexElin <alexey.elin@list.ru>
2017-10-26 22:48:37 +0600

    update JUnit

--- dbf-reader/src/test/java/org/jamel/dbf/DbfUtilsTest.java
+++ dbf-reader/src/test/java/org/jamel/dbf/DbfUtilsTest.java
@@ -6,9 +6,9 @@ import java.io.DataInputStream;
 import org.jamel.dbf.utils.DbfUtils;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
-import static junit.framework.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author Sergey Polovko
